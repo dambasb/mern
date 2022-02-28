1a36fed4aee0893d24cb0fd41b5bda902d384f53
Damir Kolobaric <dkolobaric@gmail.com>
2022-02-28 18:40:24 +0100

    form submit
    form submit

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+function Posts() {
+  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
+  return (
+    <div>
+      Posts
+      <Post />
+      <Post />
+    </div>
+  );
+}
+
+export default Posts;
---
+++ server/controllers/posts.js
@@ -0,0 +1,24 @@
+import PostMessage from "../models/postsMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMassages = await PostMessage.find();
+
+    console.log(postMassages);
+    res.status(200).json(postMassages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = async (req, res) => {
+  const post = req.body;
+
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
