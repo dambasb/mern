24642b13bb8cdb4f376a1ee767a48aacf715810c
Damir Kolobaric <dkolobaric@gmail.com>
2022-03-05 14:38:39 +0100

    add git ignore
    add git ignore

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
+app.use("/", (res, req) => {
+  res.send("This is API");
+});
 const CONNECTION_URL =
   "mongodb+srv://mern:mern@mern.kilap.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
